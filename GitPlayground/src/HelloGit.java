80414b8a3611a06b39a12bcfedddde641d7a3f3f
Training <Training@192.168.252.139>
2020-03-07 14:49:59 +0530

    VERSION1

--- GitPlayground/src/HelloGit.java
+++ GitPlayground/src/HelloGit.java
@@ -3,6 +3,6 @@ public class HelloGit {
 public static void main(String[] args) {
 	System.out.println("Hello git");
 	System.out.println("HOW ARE U");
-	System.out.println("I M FINE ");
+	System.out.println("I M FINE JAADU");
 }
 }
